cb07266b2d00003167ace8477379bc0864ffbe52
Luke Prescott <luke.prescott@presport.us>
2018-11-08 20:43:38 -0500

    First attempt at newtons method
    Calculating the solution to a system of non-linear equations using Newton's Method

---
+++ Assignment3_Problem3.py
@@ -0,0 +1,108 @@
+import math
+import numpy as np
+import sys
+
+# Luke R. Prescott
+# 11/08/2018
+# Newton's Method for solving a System of Equations
+# This program uses Gaussian Elimination to solve the given non-linear system of equations using Newton's method.
+def main():
+
+    #Given functions:
+    #f(x,y) = x^2+xy^3=9  \
+    #g(x,y) = 3x^2-y^3=4  / p(x,y)
+
+    #Manually calculated jacobian matrix:
+    #| 2x+y^3 3xy^2     |
+    #| 6xy    3x^2-3y^2 |
+
+    #Other given info:
+    initialGuess = [2.98, 0.15]
+    x_k = initialGuess[0]
+    y_k = initialGuess[1]
+    error = float(10) * math.pow(10, -12)
+
+    #Default the values of x for the next iteration.
+    x_k1 = 0
+    y_k1=0
+
+    #Print the title bar
+    print('| {:>5} | {:>25} | {:>25} | {:>25} | {:>25} | {:>25} |'.format("k", "x_k", "y_k", "x_k+1", "y_k+1", "x_k+1 - x_k"))
+
+    #Set difference, to use as a flag, as a arbitrarily high number
+    difference = 100
+
+    #For the given number of iterations
+    for count in range(0, 4):
+        
+        #For every iteration except the first
+        if(count!=0):
+
+            #A is the matrix A, the jacobian of the given system
+            A = jacobian(x_k, y_k)
+            #A_np is the numpy array equivalent
+            A_np = np.array(A)
+
+            #b is the matrix b, the negated given system
+            b = givenSystem(x_k, y_k)
+            #b_np is the numpy array equivalent
+            b_np = np.array(b)
+
+            #Call numpy's (np) method linalg to solve a linear matrix equation pass A and B
+            x = np.linalg.solve(A_np, b_np)
+
+            #set deltaX and deltaY to position x_11 and x_21 in the returned 1x2 matrix
+            deltaX = x[0][0]
+            deltaY = x[1][0]
+
+            #Increment x_k and y_k by their changed values
+            x_k += deltaX
+            y_k += deltaY
+
+        #On all iterations
+        #Find the next values for x and y for printing of table
+        x_k1 = x_k + np.linalg.solve(np.array(jacobian(x_k, y_k)), np.array(givenSystem(x_k, y_k)))[0][0]
+        y_k1 = y_k + np.linalg.solve(np.array(jacobian(x_k, y_k)), np.array(givenSystem(x_k, y_k)))[1][0]
+
+        #Calculate the difference between x_k and x_k+1
+        difference = x_k1 - x_k
+
+        #print table row
+        print('| {:>5} | {:>25} | {:>25} | {:>25} | {:>25} | {:>25} |'.format(count, x_k, y_k, x_k1, y_k1, difference))
+
+        #increment count
+        count += 1
+
+    ''' #This checkes if the found Ax=b matrix equation checks.
+    if(np.allclose(np.dot(A_np, x), b_np)):
+        print("Checked!")
+    else:
+        print("Failed!")
+    '''
+
+def jacobian(x_k, y_k):
+    #j_row,column
+    j_11 = 2 * x_k + math.pow(y_k, 3)
+    j_12 = 3 * x_k * math.pow(y_k, 2)
+    j_21 = 6 * x_k * y_k
+    j_22 = 3 * math.pow(x_k, 2) - 3 * math.pow(y_k, 2)
+    return [[j_11, j_12],[j_21, j_22]]
+
+def givenSystem(x_k, y_k):
+    #p_row,column
+    p_11 = -1 * ( math.pow(x_k,2) + x_k * math.pow(y_k, 3) - 9 )
+    p_21 = -1 * ( 3 * math.pow(x_k, 2) * y_k - math.pow(y_k, 3) - 4 )
+    return [[p_11], [p_21]]
+
+def printMatrix(matrix):
+    for row in matrix:
+        for val in row:
+            print('{:<20}'.format(val), end=' ')
+        print()
+    print()
+
+def error(x_k1, x_k):
+    return x_k1 - x_k
+            
+if __name__ == "__main__":
+    main() 
